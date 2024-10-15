35c00aa63a1e971e1841d59d4cdd8dd7a44eec6e
Zakariae Nasri <znasri@smb.sqli.com>
2024-10-15 18:07:26 +0100

    Code source

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import MainLayout from "../layouts/MainLayout";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <MainLayout>
+      <Component {...pageProps} />
+    </MainLayout>
+  );
+}
+
+export default MyApp;
